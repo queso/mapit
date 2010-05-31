15c3f63dd82b644d9cc2e2b200058472de6ab030
Josh Owens <joshua.owens@gmail.com>
2010-05-31 09:19:10 -0400

    Switch to just a yield :mapit in the header of the layouts, thus the js calls are only made when we want a map on the page.

--- app/helpers/mapit_helper.rb
+++ app/helpers/mapit_helper.rb
@@ -1,15 +1,11 @@
 module MapItHelper
   
-  def javascript_mapit_include
-    js = '<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>'
-    js += javascript_include_tag 'mapit'
-    js
-  end
-  
   def add_map(div, points, options = {})
     points = points.to_markers unless points.first.class == Hash
-    content_for :map do
+    content_for :mapit do
       <<-END
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        #{javascript_include_tag 'mapit'}
         <script type="text/javascript">
           var map_points = #{points.compact.to_json};
           addLoadEvent( function () { buildMapIt("#{div}", map_points, ""); });
