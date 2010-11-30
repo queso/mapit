07b6c4d09cec0cb9c4431ca01cb53b8566ef6699
Josh Owens <joshua.owens@gmail.com>
2010-11-30 16:03:50 -0500

    Oops, that shouldn't be a hash rocket, just an equal sign

--- app/helpers/mapit_helper.rb
+++ app/helpers/mapit_helper.rb
@@ -14,7 +14,7 @@ module MapItHelper
     end
   end
   
-  def link_to_infowindow(text, url, id, options => {})
+  def link_to_infowindow(text, url, id, options = {})
     link_to text, url, options.merge({:onclick => "infoWindow_#{id}(); return false;"})
   end
   
